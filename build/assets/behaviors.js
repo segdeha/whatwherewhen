458b75790c7c5f292ac8183c4d0c73d7161e11cc
Andrew Hedges <andrew@hedges.name>
2022-03-16 20:42:20 -0700

    Guessing my logging function wasn't working correctly

--- build/assets/behaviors.js
+++ build/assets/behaviors.js
@@ -2,11 +2,12 @@
 function writeResult(str) {
     const result = document.querySelector('#result')
     const contents = result.innerHTML
-    result.innerHTML = contents + str
+    result.innerHTML = contents + '\n' + str
 }
 
 function init() {
     writeResult('hello, world')
+    writeResult('hello, yourself')
 }
 
 init()
