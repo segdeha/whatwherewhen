fb62c988d0f86c7a16cd8378f10ac1e82b91a7f4
Andrew Hedges <andrew@hedges.name>
2022-03-16 20:45:26 -0700

    It's never as simple as it appears at first

--- build/assets/behaviors.js
+++ build/assets/behaviors.js
@@ -2,7 +2,7 @@
 function writeResult(str) {
     const result = document.querySelector('#result')
     const contents = result.innerHTML
-    result.innerHTML = contents + '\n' + str
+    result.innerHTML = contents ? contents + '\n' + str : str
 }
 
 function init() {
