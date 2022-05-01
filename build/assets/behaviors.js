2c43b86a49b6662fea5f82155685cd6d0bd65321
Andrew Hedges <andrew@hedges.name>
2022-05-01 11:55:20 -0700

    show current day of the week

--- build/assets/behaviors.js
+++ build/assets/behaviors.js
@@ -1,9 +1,11 @@
 import initMap from './map.js';
 import { initData } from './logger.js';
+import todaysDay from './days.js'
 
 function init() {
     initData()
     initMap()
+    document.querySelector('h1').innerHTML = todaysDay()
 }
 
 init()
