14b9a15729e07b87a1c01792070e22f19d6cd83c
Andrew Hedges <andrew@hedges.name>
2022-05-02 17:04:39 -0700

    ui tweaks

--- build/assets/behaviors.js
+++ build/assets/behaviors.js
@@ -3,7 +3,9 @@ import { initData } from './logger.js';
 import todaysDay from './days.js'
 
 function initUI() {
-    document.querySelector('h1').innerHTML = todaysDay()
+    let today = todaysDay()
+    let cappedDay = today[0].toUpperCase() + today.substring(1)
+    document.querySelector('h1').innerHTML = `Where are they on a ${cappedDay}?`
 }
 
 function init() {
