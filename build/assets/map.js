df52926115b4b81991ecedb4dc27376c596e06d5
Andrew Hedges <andrew@hedges.name>
2022-07-09 01:10:22 -0700

    tentative solution for showing direction of travel

--- build/assets/map.js
+++ build/assets/map.js
@@ -17,7 +17,10 @@ const state = {
     vertices: [],
     meMarker: null,
     polygon: null,
-    lastCoords: { lat: 45.53, lng: -122.63 }
+    lastCoords: { lat: 45.531714, lng: -122.630676 }, // NE 33rd & Sandy
+    // lastCoords: { lat: 45.531549, lng: -122.632114 }, // sw of home
+    // lastCoords: { lat: 45.533961, lng: -122.627865 }, // ne of home
+    direction: 'n', // 's', 'e', 'w', 'nw', 'ne', 'sw', 'se'
 }
 
 function zoom() {
@@ -31,10 +34,11 @@ function followMe() {
     navigator.geolocation.watchPosition(pan, console.log)
     function pan(loc) {
         const { latitude, longitude } = loc.coords
-        state.lastCoords = { lat: latitude, lng: longitude }
         // always move the user whether or not we’re following them
         moveMeMarker(latitude, longitude)
-        panToCoords(latitude, longitude)
+        // set state after we move the marker so that function can
+        // also set the direction
+        state.lastCoords = { lat: latitude, lng: longitude }
     }
 }
 
@@ -42,21 +46,98 @@ function panToCoords(lat, lng) {
     state.map.panTo(new google.maps.LatLng(lat, lng))
 }
 
-function makeMeMarker(lat, lng) {
-    const position = new google.maps.LatLng(lat, lng)
+// Converts from degrees to radians.
+function toRadians(degrees) {
+  return degrees * Math.PI / 180;
+}
+
+// Converts from radians to degrees.
+function toDegrees(radians) {
+  return radians * 180 / Math.PI;
+}
+
+// from: https://stackoverflow.com/a/52079217
+// i don't understand it, but it works
+function bearing(startLat, startLng, destLat, destLng){
+  startLat = toRadians(startLat);
+  startLng = toRadians(startLng);
+  destLat = toRadians(destLat);
+  destLng = toRadians(destLng);
+
+  const y = Math.sin(destLng - startLng) * Math.cos(destLat);
+  const x = Math.cos(startLat) * Math.sin(destLat) -
+        Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng);
+  let brng = Math.atan2(y, x);
+  brng = toDegrees(brng);
+  return (brng + 360) % 360;
+}
+
+function setDirection(lat, lng) {
+    // compare passed in lat, lng with state.lastCoords
+    // to determine which way the icon should face
+    const brng = bearing(state.lastCoords.lat, state.lastCoords.lng, lat, lng)
+
+    // 0 - 22.5 ... 0 + 22.5
+    if (brng > -22.5 && brng < 22.5) {
+        state.direction = 'n'
+    }
+    // 180 - 22.5 ... 180 + 22.5
+    else if (brng > 157.5 && brng < 202.5) {
+        state.direction = 's'
+    }
+    // 90 - 22.5 ... 90 + 22.5
+    else if (brng > 67.5 && brng < 112.5) {
+        state.direction = 'e'
+    }
+    // 270 - 22.5 ... 270 + 22.5
+    else if (brng > 247.5 && brng < 292.5) {
+        state.direction = 'w'
+    }
+    // 45 - 22.5 ... 45 + 22.5
+    else if (brng > 22.5 && brng < 67.5) {
+        state.direction = 'ne'
+    }
+    // 315 - 22.5 ... 315 + 22.5
+    else if (brng > 292.5 && brng < 327.5) {
+        state.direction = 'nw'
+    }
+    // 135 - 22.5 ... 135 + 22.5
+    else if (brng > 112.5 && brng < 157.5) {
+        state.direction = 'se'
+    }
+    // 202.5 - 22.5 ... 202.5 + 22.5
+    else {
+        state.direction = 'sw'
+    }
+
+    document.querySelector('#debug').innerHTML = document.querySelector('#debug').innerHTML + `${state.direction}<br>`
+
+}
+
+function getIcon() {
     const icon = {
-        url: '/assets/img/me.png',
+        url: `/assets/img/me-${state.direction}.png`,
         scaledSize: new google.maps.Size(16, 16),
         origin: new google.maps.Point(0,0),
         anchor: new google.maps.Point(0, 0),
     }
+    return icon
+}
+
+function makeMeMarker(lat, lng) {
+    const position = new google.maps.LatLng(lat, lng)
+    const icon = getIcon()
     const { map } = state
     state.meMarker = new google.maps.Marker({ position, icon, map })
 }
 
 function moveMeMarker(lat, lng) {
     const newPosition = new google.maps.LatLng(lat, lng)
+    setDirection(lat, lng)
+    panToCoords(lat, lng)
     state.meMarker.setPosition(newPosition)
+    const icon = getIcon()
+    state.meMarker.setIcon(icon)
 }
 
 function drawPolygon() {
@@ -131,7 +212,7 @@ function addPoint(mapsMouseEvent) {
 }
 
 function initMap() {
-    const center = { lat: 45.53, lng: -122.63 }
+    const center = state.lastCoords
     const { lat, lng } = center
     // new map, centered on NE 33rd & Sandy
     state.map = new google.maps.Map(document.getElementById("map"), {
